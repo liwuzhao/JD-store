53698a121667414a7e4789319564c6821dd1a3c5
wuzhao <liwuzhao97@gmail.com>
2017-05-27 09:44:41 +0800

    bug

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,5 +1,5 @@
 class CartItemsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user! , only: [:checkout]
 
   def destroy
     @cart = current_cart
