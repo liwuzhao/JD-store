0e3260543759b029ff042c58053fd9db9edc8447
wuzhao <liwuzhao97@gmail.com>
2017-06-10 12:51:39 +0800

    avatar

--- db/migrate/20170610042718_remove_avatar_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAvatarFromUser < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :avatar
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610042918) do
+ActiveRecord::Schema.define(version: 20170609140607) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 20170610042918) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "avatar"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
@@ -135,7 +136,6 @@ ActiveRecord::Schema.define(version: 20170610042918) do
     t.string   "nickname"
     t.string   "address"
     t.string   "numbers"
-    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
