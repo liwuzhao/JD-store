76d0ba4aca79ee7ffcbf47db4ad3330814498bd4
wuzhao <liwuzhao97@gmail.com>
2017-06-26 10:32:14 +0800

    buy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,17 +29,29 @@ class ProductsController < ApplicationController
   def add_to_cart
     @quantity = params[:quantity].to_i
 
-    if current_cart.products.include?(@product)
-      # flash[:warning] = "你的购物车内已有此物品"
-    elsif @quantity <= @product.quantity # 如果输入的数量小于库存
-      current_cart.add_product_to_cart(@product, @quantity)
-      # flash[:notice] = "您选择的商品已加入购物车"
-    else
-      # flash[:warning] = "您选择的商品数量超过库存"
+    if params[:commit] == "加入购物车"
+      if current_cart.products.include?(@product)
+        redirect_to product_path(@product)
+        flash[:warning] = "你的购物车内已有此物品"
+      elsif @quantity <= @product.quantity # 如果输入的数量小于库存
+        current_cart.add_product_to_cart(@product, @quantity)
+        flash[:notice] = "成功加入购物车"
+        redirect_to product_path(@product)
+      else
+        redirect_to product_path(@product)
+        flash[:warning] = "库存不足"
+      end
+
+    elsif params[:commit] == "立即购买"
+      if !current_cart.products.include?(@product)
+        current_cart.add_product_to_cart(@product, @quantity)
+        redirect_to carts_path
+      else
+        flash[:warning] = "你的购物车内已有此物品"
+        redirect_to carts_path
+      end
     end
 
-    # redirect_to product_path(@product)
-    render "add_to_cart"
   end
 
   def pay_now
