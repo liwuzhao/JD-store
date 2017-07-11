a5548962bbb2e66b433a7690edb24f2903290f5b
wuzhao <liwuzhao97@gmail.com>
2017-07-11 23:53:49 +0800

    chart

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,12 +5,39 @@ class Admin::OrdersController < ApplicationController
   before_action :admin_required
 
   def index
-    @orders = Order.order("id DESC")
+    @orders = Order.all
+    colors = ['rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+              ]
+
+     if @orders.any?
+       dates = (@orders.order("id ASC").first.created_at.to_date..Date.today).to_a
+
+       @data3 = {
+         labels: dates,
+         datasets:
+           [{
+             :label => "Orders",
+             :data => dates.map{ |d|
+               @orders.where( "created_at >= ? AND created_at <= ?", d.beginning_of_day, d.end_of_day).count
+             },
+             borderColor: colors
+           }]
+       }
+     end
   end
 
   def show
     @order = Order.find(params[:id])
     @product_lists = @order.product_lists
+
+
+
+
   end
 
   def activate
