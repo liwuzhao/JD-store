f40ece57ba767ad24ff130d9f42b26d4cfb539a8
wuzhao <liwuzhao97@gmail.com>
2017-08-17 16:16:23 +0800

    find_or_create_cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,12 +27,13 @@ class ApplicationController < ActionController::Base
   private
 
   def find_session_cart
-    cart = Cart.find_by(id: session[:cart_id])
-    if cart.blank?
-      cart = Cart.create
-    end
-    session[:cart_id] = cart.id
-    cart
+    # cart = Cart.find_by(id: session[:cart_id])
+    # if cart.blank?
+    #   cart = Cart.create
+    # end
+    # session[:cart_id] = cart.id
+    # cart
+    cart = Cart.find_or_create_by(id: session[:cart_id])
   end
 
   def find_user_cart
